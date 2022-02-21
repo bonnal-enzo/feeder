9e571fac0f15e87e66d62f55473e33343acdb3a9
e.bonnal <enzo.bonnal@dailymotion.com>
2022-02-21 14:10:55 +0100

    usable mvp

--- feeder/__main__.py
+++ feeder/__main__.py
@@ -1,20 +1,21 @@
 import time
 import sys
+import logging
 from pynput.mouse import Button, Controller
 
 class ClicPoint:
 
     mouse = Controller()
+    actions_delay_seconds = 1
 
-    def __init__(self, coords, actions_delay_seconds = 1):
+    def __init__(self, coords):
         self.coords = coords
-        self.actions_delay_seconds = actions_delay_seconds
     
     def delay(self):
         time.sleep(self.actions_delay_seconds)
 
     def point_at(self):
-        self.mouse.position = self.coords
+        self.mouse.position = self.coords.tuple
         self.delay()
 
     def clic(self):
@@ -39,6 +40,7 @@ class Coords:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.tuple = (x, y)
 
     def minus(self, coords):
         return Coords(self.x - coords.x, self.y - coords.y)
@@ -65,18 +67,15 @@ class ClicPointFactory:
     def __init__(self, equip_tab_x_coord, resou_tab_x_coord):
         if equip_tab_x_coord is None or resou_tab_x_coord is None:
             self.coef = 1  # TODO remove, dev only
+            self.origin = self.ref_origin
         else:
             self.coef = (resou_tab_x_coord - equip_tab_x_coord) / self.ref_tabs_x_diff
-        self.origin = equip_tab_x_coord.minus(self.ref_equip_tab_relative_coords.times(self.coef))
-    
-    def __init__(self, equip_tab_x_coord, resou_tab_x_coord):
-        self.coef = (resou_tab_x_coord - equip_tab_x_coord) / self.ref_tabs_x_diff
-        self.origin = equip_tab_x_coord.minus(self.ref_equip_tab_relative_coords.times(self.coef))
+            self.origin = equip_tab_x_coord.minus(self.ref_equip_tab_relative_coords.times(self.coef))
     
     def _true_coords_from_ref(self, coords):
-        coords.times(self.coef).plus(self.origin)
+        return coords.times(self.coef).plus(self.origin)
 
-    def createFamiInvSlots(self, n_famis):
+    def create_fami_inv_slots(self, n_famis):
         return [
             ClicPoint(
                 self._true_coords_from_ref(self.ref_first_inv_slot_relative_coords)
@@ -90,25 +89,25 @@ class ClicPointFactory:
             for i in range(n_famis)
         ]
     
-    def createEquipmentTab(self):
+    def create_equipment_tab(self):
         return ClicPoint(self._true_coords_from_ref(self.ref_equip_tab_relative_coords))
 
-    def createResourcesTab(self):
+    def create_resources_tab(self):
         return ClicPoint(self._true_coords_from_ref(self.ref_resou_tab_relative_coords))
     
-    def createFamiSlot(self):
+    def create_fami_slot(self):
         return ClicPoint(self._true_coords_from_ref(self.ref_fami_slot_relative_coords))
 
-    def createFoodInvSlot(self):
+    def create_food_inv_slot(self):
         return ClicPoint(self._true_coords_from_ref(self.ref_first_inv_slot_relative_coords))
 
 class FeedStrategy:
     def __init__(self, clic_point_factory: ClicPointFactory, n_famis: int):
-        self.fami_inv_slots: iter[ClicPoint] = clic_point_factory.createFamiInvSlots(n_famis)
-        self.equip_tab: ClicPoint = clic_point_factory.createEquipmentTab()
-        self.res_tab: ClicPoint = clic_point_factory.createResourcesTab()
-        self.fami_slot: ClicPoint = clic_point_factory.createFamiSlot()
-        self.food_inv_slot: ClicPoint = clic_point_factory.createFoodInvSlot()
+        self.fami_inv_slots: iter[ClicPoint] = clic_point_factory.create_fami_inv_slots(n_famis)
+        self.equip_tab: ClicPoint = clic_point_factory.create_equipment_tab()
+        self.res_tab: ClicPoint = clic_point_factory.create_resources_tab()
+        self.fami_slot: ClicPoint = clic_point_factory.create_fami_slot()
+        self.food_inv_slot: ClicPoint = clic_point_factory.create_food_inv_slot()
 
     def feed(self):
         self.equip_tab.clic()
@@ -123,11 +122,14 @@ class FeedStrategy:
 
 class Main:
     def run(feed_strategy: FeedStrategy):
-        time.sleep(3)
+        ClicPoint.actions_delay_seconds = 0.5
+        time.sleep(2)
         feed_strat.feed()
 
 
 if __name__ == "__main__":
     clic_point_factory = ClicPointFactory(None, None)
-    feed_strat = FeedStrategy(clic_point_factory, int(sys.argv[1]))
+    n_famis = int(sys.argv[1])
+    feed_strat = FeedStrategy(clic_point_factory, n_famis)
+    logging.info("Entering run with n_famis=", n_famis)
     Main.run(feed_strat)
